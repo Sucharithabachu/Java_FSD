33bb61ed1167d0e48eb4b99657d61821e52a84e1
Cherry <sucharithashetty2002@gmail.com>
2022-09-03 19:46:40 +0530

    Phase 3 Project

--- Phase 3/Phase 3 Final Project/OnlineQuizPortal/src/main/java/com/simplilearn/demo/AnswerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.simplilearn.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnswerRepo extends JpaRepository<Answer, Integer>{
-
-}
--- Phase 3/Phase 3 Final Project/OnlineQuizPortal/src/main/java/com/simplilearn/demo/ParticipantRepo.java
+++
@@ -1,7 +0,0 @@
-package com.simplilearn.demo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParticipantRepo extends JpaRepository<Participant, Integer>{
-}
